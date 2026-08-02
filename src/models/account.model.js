18671fe7af111ddfe97c8559d8633ab75b8ad7b3
sayamcodezzz <sayamkhajuria@gmail.com>
2026-08-02 23:27:58 +0530

    create account models

---
+++ src/models/account.model.js
@@ -0,0 +1,33 @@
+const mongoose= require("mongoose");
+
+const accountSchema=  new mongoose.Schema({
+    user:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"user",
+        required:[true,"Account must be associated with a user"],
+        index:true, 
+
+    },
+    status:{
+        enum:{
+            values:["ACTIVE","CLOSED","FROZEN"],
+            message:"Status must be either ACTIVE , FROZEN or CLOSED",
+            default:"ACTIVE",
+        },
+    },
+    currency:{
+        type:String,
+        required:[true,"Currency is required for creating an account"],
+        default:"INR"
+    },
+    
+},{
+    timestamps:true,
+})
+
+
+const accountModel= mongoose.model("account",accountSchema)
+
+module.exports ={
+    accountModel,
+}
